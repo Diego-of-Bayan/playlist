c680cf854bf54cea93456f4304249cfb593e333e
Diego Ramil <dbramil811@gmail.com>
2019-02-04 23:15:36 +0000

    continued JS

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 /* global $ */
-
-// BELOW Update the songs array with four of your favorites songs.
 let imageLinks = ["https://i.ytimg.com/vi/djV11Xbc914/hqdefault.jpg", "https://i.ytimg.com/vi/xB9MLUAWkAw/maxresdefault.jpg", "https://www.biography.com/.image/t_share/MTE4MDAzNDEwNjg4MTE2MjM4/frank-sinatra-9484810-3-402.jpg", "https://lwlies.com/wp-content/uploads/2018/06/george-michael-freedom-film-1108x0-c-default.jpg", "https://i.scdn.co/image/7da0877636b199b27e64ee117f71f5b97758349a", "https://images.indianexpress.com/2016/11/mission-impossible-759.jpg", "https://img.taste.com.au/hyE5dSN3/taste/2016/11/spaghetti-with-meatballs-and-spicy-tomato-sauce-102298-1.jpeg"];
 let songs = ["Take On Me", "Spanish Flea", "Fly Me To The Moon", "Careless Whisper", "Yakety Sax", "Mission Impossible (Corrupted)", "ASMR (I'm Sorry)"];
 let artists = ["A-Ha", "Herb Alpert", "Frank Sinatra", "George Michael", "Unknown", "Adam Clayton and Larry Mullen Jr.", "BlovesASMR Eating Her Way"];
 let songLengths = ["3:47", "60:00", "2:28","5:01:", "4:34", "0:47", "16:16"];
 let links = ["https://www.youtube.com/watch?v=djV11Xbc914", "https://www.youtube.com/watch?v=1sruEnQ9HkU", "https://www.youtube.com/watch?v=5hxibHJOE5E", "https://www.youtube.com/watch?v=izGwDsrQ1eQ", "https://www.youtube.com/watch?v=ZnHmskwqCCQ", "https://www.youtube.com/watch?v=Erm8wJVXVao", "https://www.youtube.com/watch?v=PMCA5znLBD0"];
-// BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
-// Make sure they match the same order as your array above
 let allArrays = [imageLinks, songs, artists, songLengths, links];
-// BELOW Use forEach Loop to display the data from each of your array's in the correct div
 let elem1 = $("#images");
 let elem2 = $("#songs");
 let elem3 = $("#artists");
@@ -21,28 +16,19 @@ for (let i = 0; i < allArrays.length; i++) {
         if (i === 0) {
             elems[i].append("<img src='" + element + "'>");
         } else if (i === 4) {
-            elems[i].append("<a href='" + element + "'>Link</a>")
+            elems[i].append("<a href='" + element + "'>Link</a>");
         } else {
-            elems[i].append("<h1>" + element + "</h1>")
+            elems[i].append("<h1>" + element + "</h1>");
         }
     });
 }
-function emptySongInfo(){
-    $("#songs").empty();
-    // Use jQuery to empty all of the remaining divs
-}
-
-
 function addSongInfo(){
-    // BELOW write the code to add new items to each of the arrays.
-
-
+    imageLinks.unshift($("#image").val());
+    songs.unshift($("#song").val());
+    artists.unshift($("#artist").val());
+    songLengths.unshift($("#length").val());
+    links.unshift($("link").val());
 }
-
 $("#add").click(function() {
-    emptySongInfo();
     addSongInfo();
-    displaySongInfo();
 });
-
-displaySongInfo();
